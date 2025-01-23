635e34733b61dc19eab27ec8ef7023c18983cca7
黃俊誠 <s111060003@m111.nthu.edu.tw>
2025-01-23 12:22:30 +0800

    Update ParityGenerator.v

--- ParityGenerator.v
+++ ParityGenerator.v
@@ -1,25 +1,12 @@
 module ParityGenerator(
-  input  wire [15:0] a_i,
-  output reg  [15:0] parity_o
+  input wire [15:0] a_i,
+  output reg [15:0] parity_o
 );
-
   always @(*) begin
-    parity_o = a_i[0];
-    parity_o = parity_o ^ a_i[1];
-    parity_o = parity_o ^ a_i[2];
-    parity_o = parity_o ^ a_i[3];
-    parity_o = parity_o ^ a_i[4];
-    parity_o = parity_o ^ a_i[5];
-    parity_o = parity_o ^ a_i[6];
-    parity_o = parity_o ^ a_i[7];
-    parity_o = parity_o ^ a_i[8];
-    parity_o = parity_o ^ a_i[9];
-    parity_o = parity_o ^ a_i[10];
-    parity_o = parity_o ^ a_i[11];
-    parity_o = parity_o ^ a_i[12];
-    parity_o = parity_o ^ a_i[13];
-    parity_o = parity_o ^ a_i[14];
-    parity_o = parity_o ^ a_i[15];
+    parity_o = a_i[0] ^ a_i[1] ^ a_i[2] ^ a_i[3] ^ 
+               a_i[4] ^ a_i[5] ^ a_i[6] ^ a_i[7] ^ 
+               a_i[8] ^ a_i[9] ^ a_i[10] ^ a_i[11] ^ 
+               a_i[12] ^ a_i[13] ^ a_i[14] ^ a_i[15];
   end
 
 endmodule
